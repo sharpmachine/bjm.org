c00b69c44b94679f945874096458a582f7a79bb3
bjaalberts <bjaalberts@gmail.com>
2013-04-03 11:01:37 -0700

    finished contact page

--- wp-content/themes/bjm/page-contact.php
+++ wp-content/themes/bjm/page-contact.php
@@ -25,47 +25,15 @@ get_header(); ?>
 <div class="send-a-message boxy">
 	<div class="container">
 		<div class="row">
-			<div class="span6">
-				<h4>Send A Message</h4>
-				<form class="form-horizontal">
-				  <div class="control-group">
-				    <label class="control-label" for="inputName">Name</label>
-				    <div class="controls">
-				      <input type="text" placeholder="">
-				    </div>
-				  </div>
-				  <div class="control-group">
-				    <label class="control-label" for="inputEmail">Email</label>
-				    <div class="controls">
-				      <input type="email">
-				    </div>
-				  </div>
-				  <div class="control-group">
-				    <label class="control-label" for="inputSubject">Subject</label>
-				    <div class="controls">
-				      <select>
-							  <option>Prayer Request</option>
-							  <option>Bill's Travel</option>
-							  <option>Schedule</option>
-							  <option>Website Feedback</option>
-							  <option>Media Sales</option>
-							  <option>General info or Comments</option>
-							</select>
-				    </div>
-				  </div>
-			</div>
-			<div class="span6">
-				<div class="control-group">
-					<label class="control-label" for="inputMessage">Message</label>
-					<div class="controls">
-						<textarea name="" id="" cols="30" rows="5"></textarea>
-					</div>
-				</div>
-				<button type="submit" class="btn pull-right">
-					Submit
-				</button>
-			</div>
-			</form>
+			
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<?php the_content(); ?>
+				<?php endwhile; ?>
+				<!-- post navigation -->
+				<?php else: ?>
+				<!-- no posts found -->
+				<?php endif; ?>
+
 		</div>
 	</div>
 </div>
