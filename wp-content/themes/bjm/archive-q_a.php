2496c5e51498ef1172340826ce8b90efe080f84c
Jesse Kade <jessekade@Sharp-Machine.local>
2013-04-03 17:19:59 -0700

    fixed qa and contact messages

--- wp-content/themes/bjm/archive-q_a.php
+++ wp-content/themes/bjm/archive-q_a.php
@@ -10,16 +10,10 @@
 			<div class="span6 ask-container">
 				<div class="align">
 					<h2>Have a question?</h2>
-<<<<<<< HEAD
+
 					<div class="ask">Go ahead and ask! Check back to see if we answered it.</div>
 					<?php echo do_shortcode( '[contact-form-7 id="1114" title="Ask A Question"]' ); ?>
-=======
-					<div class="ask">Go ahead and ask! If we answer it we'll notify you.</div>
-						<form action="">
-							<input type="text">
-							<button type="submit" class="btn">Submit</button>
-						</form>
->>>>>>> ca4e126d79c19768a8b5830baa2a10847b950e2a
+
 				</div>
 			</div>
 		</div>
@@ -31,7 +25,7 @@
 		<div class="row">
 		
 			<div class="span4 qa-sidebar visible-desktop">
-				<div class="affix-top" data-spy="affix" data-offset-top="460">
+				<div class="affix-top" data-spy="affix" data-offset-top="525">
 				<?php query_posts( 'post_type=q_a' ); ?>
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 					
