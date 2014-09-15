5972a2eeaabd7abd1932e964b9e8b4f186a74ff7
Jesse Kade <jessekade@gmail.com>
2014-09-15 14:11:22 -0700

    Clean up page schedule

--- wp-content/themes/bjm/page-schedule.php
+++ wp-content/themes/bjm/page-schedule.php
@@ -2,32 +2,24 @@
 /*
 	Template Name: Schedule
 */
-get_header(); ?>
+	get_header(); ?>
 
-<div class="schedule">
-	<div class="container">
+	<div class="schedule">
+		<div class="container">
 
-		<div class="row">
-			<div class="span12">
-				<img src="<?php bloginfo('template_directory'); ?>/img/schedule-image.png" alt="Schedule">
+			<div class="row">
+				<div class="span12">
+					<img src="<?php bloginfo('template_directory'); ?>/img/schedule-image.png" alt="Schedule">
+				</div>
 			</div>
-		</div>
-		
-		<div class="row">
-			<div class="span12 bethel-schedule">
-			
-				<!-- <iframe class="visible-desktop" style="background: none; width: 100%; height: 5100px" src="http://www.ibethel.org/api?t=itinerary&amp;author=billjohnson&amp;styles=<?php bloginfo('template_directory'); ?>/css/itinerary.css" height="100%" width="100%" scrolling="no" frameborder="0"></iframe>
-				
-				<iframe class="visible-tablet" style="background: none; width: 100%; height: 5150px;" src="http://www.ibethel.org/api?t=itinerary&amp;author=billjohnson&amp;styles=<?php bloginfo('template_directory'); ?>/css/itinerary-tablet.css" height="100%" width="100%" scrolling="no" frameborder="0"></iframe>
 
-				<iframe class="visible-phone" style="background: none; width: 100%; height: 3210px;" src="http://www.ibethel.org/api?t=itinerary&amp;author=billjohnson&amp;styles=<?php bloginfo('template_directory'); ?>/css/itinerary-phone.css" height="100%" width="100%" scrolling="yes" frameborder="0"></iframe> -->
-				<div id="itineraries">Loading trips...</div>
-
-
+			<div class="row">
+				<div class="span12 bethel-schedule">					
+					<div id="itineraries">Loading trips...</div>
+				</div>
 			</div>
-		</div>
-		
-	</div><!-- .container -->
-</div>
 
-<?php get_footer(); ?>
+		</div><!-- .container -->
+	</div>
+
+	<?php get_footer(); ?>
